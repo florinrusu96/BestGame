c0499f50c00f2030f298298bb83431fbe950c069
mehai <mihaizanfir98@gmail.com>
2018-12-27 17:04:28 +0200

    create libGDX FlappyBird project

---
+++ FlappyBird/desktop/src/com/bestgame/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bestgame.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bestgame.flappybird.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
