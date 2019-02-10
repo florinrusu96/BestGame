861eb61581c8c51c16488df9a6b6f759e0422f92
mehai <mihaizanfir98@gmail.com>
2019-02-10 11:51:43 +0200

    create libGDX project

---
+++ Menus/desktop/src/com/bestgame/menus/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bestgame.menus.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bestgame.menus.BestGameMenus;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BestGameMenus(), config);
+	}
+}
