9a2a09bf518e098de3035ec3a5484aab81098a70
Unknown <damian.robert.andrei@gmail.com>
2019-02-10 19:23:00 +0200

    Creat Pacman

---
+++ PacMan/desktop/src/best/bestem/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package best.bestem.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import best.bestem.pacman.PacManGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 400;
+		config.height = 720;
+		new LwjglApplication(new PacManGame(), config);
+	}
+}
