b937a540e44ab6aadf602a3e5ee154f97fd40a44
Luca <luca6011@gmail.com>
2019-02-15 19:58:23 +0200

    add schelet

---
+++ BESTEM_GAME/core/src/com/mygdx/game/PacMan/PacManGame.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.PacMan;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class PacManGame extends Game {
+    SpriteBatch batch;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        this.setScreen(new best.bestem.pacman.Board(this));
+    }
+
+    @Override
+    public void render () {
+        super.render();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        getScreen().dispose();
+    }
+}
